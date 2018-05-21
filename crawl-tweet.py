70153511a528bd97b0e392d17b0e4d17f787b3df
Mario Cho <hephaex@gmail.com>
2018-05-21 19:08:01 +0900

    add pandas dataframe

--- crawl-tweet.py
+++ crawl-tweet.py
@@ -37,3 +37,16 @@ print("5 recent tweets:\n")
 for tweet in tweets[:5]:
     print(tweet.text)
     print()
+
+# Pandas Dataframe
+data = pd.DataFrame(data=[tweet.text for tweet in tweets], columns = ['Tweets'])
+
+# add relevent info to pandas dataframe
+data['len']  = np.array([len(tweet.text) for tweet in tweets])
+data['ID']   = np.array([tweet.id for tweet in tweets])
+data['Date'] = np.array([tweet.created_at for tweet in tweets])
+data['Source'] = np.array([tweet.source for tweet in tweets])
+data['Likes']  = np.array([tweet.favorite_count for tweet in tweets])
+data['RTs']    = np.array([tweet.retweet_count for tweet in tweets])
+
+print(data.head(10))
